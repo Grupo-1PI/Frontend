609f5e0206b0afe1fe954c0528da75af895f307a
Ally4wada <ally.awada@sptech.school>
2026-05-09 19:00:45 -0300

    feature: configurando cores do tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2E3A2E",
+        secondary: "#E7DEC9",
+        accent: "#D4B483",
+        textDark: "#1A1A1A",
+      },
+      fontFamily: {
+        'shoulders': '"Biig Shoulders Display"',
+        'lexend': '"Lexend Deca"',
+      },
+    },
+    plugins: [],
+  }
+}
