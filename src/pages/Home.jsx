d703f28f954d635b63a09f4cfeed81865d77d2cd
Ally4wada <ally.awada@sptech.school>
2026-05-09 18:50:22 -0300

    feature: páginas inciais adicionadas

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ArrowLeft } from "lucide-react";
+import { Link } from 'react-router-dom';
+function Home() {
+  return (
+    <div>
+      home
+    </div>
+  )
+}
+
+export default Home
